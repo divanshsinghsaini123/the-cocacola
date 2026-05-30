5b4c211158cf41f47e7be103171b2379ae72b9d9
Divansh Singh <divanshsinghsaini123@gmail.com>
2026-05-30 08:42:40 -0800

    fixed the backup thing

--- app/api/admin/cron/file_backup/route.ts
+++ app/api/admin/cron/file_backup/route.ts
@@ -9,7 +9,7 @@ export async function POST() {
                 Accept: "application/vnd.github+json",
             },
             body: JSON.stringify({
-                ref: "main",
+                ref: process.env.GITHUB_BRANCH || "digitaloccean",
             }),
         }
     );
